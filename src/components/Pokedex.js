7f1f0dd0554c04604a5d62dfb6e8e6523120954a
Ameya19 <ameya.bhingarkar19@gmail.com>
2025-01-20 22:36:34 +0530

    Added search functionality.

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -10,17 +10,21 @@ import InfoIcon from '@mui/icons-material/Info';
 import axios from 'axios';
 import Footer from './footer.js';
 import { useNavigate } from "react-router-dom";
-import TextField from '@mui/material/TextField';
 import { Helmet } from "react-helmet";
 import useMediaQuery from '@mui/material/useMediaQuery';
+import SearchIcon from '@mui/icons-material/Search';
+import Autocomplete from '@mui/joy/Autocomplete';
 
 function Pokedex() {
 
     const [pokemons, setPokemons] = useState([]);
+    const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [search, setSearch] = useState("");
     const navigate = useNavigate();
     const isSmallScreen = useMediaQuery('(max-width:600px)');
     const isMediumScreen = useMediaQuery('(max-width:960px)');
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -35,6 +39,7 @@ function Pokedex() {
                     })
                 );
                 console.log(detailedData);
+                setPosts(results);
                 setPokemons(detailedData);
                 setLoading(false);
             } catch (error) {
@@ -45,6 +50,10 @@ function Pokedex() {
         fetchData();
     }, []);
 
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+
     const getImageListItemSize = () => {
         if (isSmallScreen) {
             return { width: '45vw', height: '30vh' };
@@ -54,6 +63,16 @@ function Pokedex() {
             return { width: '15vw', height: '25vh' };
         }
     };
+
+    const searchOnClick = (search) => {
+        return () => {
+            const pokemon = pokemons.find((pokemon) => pokemon.name === search);
+            if (pokemon) {
+                navigate('/pokedex/' + pokemon.id + '/');
+            }
+        }
+    };
+
     return (
         <div id="pokedex">
             <Helmet>
@@ -63,7 +82,15 @@ function Pokedex() {
                 <PokedexAppBar />
             </div>
             <div className='search-bar'>
-                <TextField sx={{alignItems:"right"}}error id="outlined-error" label="Pokemon" />
+            <Autocomplete
+                className="search-bar"
+                placeholder="Pokemon"
+                type="search"
+                freeSolo
+                disableClearable
+                onChange={(event, value) => setSearch(value.toLowerCase())}
+                options={posts.map((post) => capitalizeFirstLetter(post.name))}
+                /><SearchIcon fontSize="large" onClick={searchOnClick(search)} sx={{color:"White", marginTop:"1%", marginLeft:"2%"}}/>
             </div>
             <div className="pokedex-body" style={{ height: isSmallScreen ? "70vh" : '68vh', overflowY: 'scroll' }}>
             {loading ? (
