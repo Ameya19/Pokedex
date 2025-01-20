aaca35db8f5cf0c07b60f787660f9026380caf7c
Ameya <51057639+Ameya19@users.noreply.github.com>
2025-01-20 17:26:10 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 function footer() {
     return(
         <div className="footer">
-                <p>Made by Ameya with love <FavoriteIcon fontSize='small'/>. </p>
+                <p>Made with love <FavoriteIcon fontSize='small'/>. </p>
         </div>
     );
 }
